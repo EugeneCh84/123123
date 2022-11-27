a8de92830ec5d735d5d3b14844ac87746f34b67a
eugene <eugene.cherepanov.84>
2022-11-27 16:44:28 +0500

    writing a programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void Zadacha ()
+{
+string[] arr = {"dslfkjsl", "req", "dejhf", "jdj", "dfsad"};
+string[] arr_result = new string[arr.Length];
+     for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3)
+                {
+                    arr_result[i] = arr[i];
+                    Console.WriteLine(arr_result[i]);
+                }
+        }              
+}
+Zadacha();
